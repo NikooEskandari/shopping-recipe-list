2203d33696fa8f71208cb522ac0534ef52b92504
Nikoo <ni.eskandari@gmail.com>
2022-03-01 21:42:35 -0500

    Commit 2

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("A Test Recipe", "This is symply a test"
+    , "https://static.onecms.io/wp-content/uploads/sites/9/2021/12/20/sous-vide-pork-chops-FT-RECIPE1221.jpg"),
+    new Recipe("A Test Recipe", "This is symply a test"
+    , "https://static.onecms.io/wp-content/uploads/sites/9/2021/12/20/sous-vide-pork-chops-FT-RECIPE1221.jpg"),
+    
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
